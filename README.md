232dcaa3e6b547c749e67b6b0deb2f6cd5fe454a
nmajuterence <nmajuterence@gmail.com>
2024-12-15 15:43:25 +0100

    0. Basic Arithmetic Exercise

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# alx_be_python
+# Introduction to Python
+
+This project introduces Python programming, covering its applications, development environment setup, and basic syntax, with a focus on readability and proper indentation.
---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,15 @@
+# this script performs simple arithmetic operations 
+number1 = 10
+number2 = 5
+
+# addition
+addition = number1 + number2
+print('Addition of {} and {} is {}.'.format(number1, number2, addition))
+
+# subtraction
+subtraction = number1 - number2
+print('Subtraction of {} and {} is {}.'.format(number1, number2, subtraction))
+
+# multiplication
+multiplication = number1 * number2
+print('Multiplication of {} and {} is {}.'.format(number1, number2, multiplication))
