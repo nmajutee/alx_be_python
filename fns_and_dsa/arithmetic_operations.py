e50e110ddc120f1e1b5c0d3f49d2e8d9c660208d
nmajuterence <nmajuterence@gmail.com>
2025-01-14 18:44:09 +0100

    0. Arithmetic Operations Function

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -9,9 +9,9 @@ def perform_operation(num1, num2, operation):
     elif operation == 'multiply':
         return num1 * num2
     elif operation == 'divide':
-        if num2 != 0:
-            return num1 / num2
-        else:
+        if num2 == 0:
             return 'zero divition error'
+        else:
+            return num1 / num2
     else:
         return 'invalid operation'
