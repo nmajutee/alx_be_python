be17b97d2b3bcd76b5ef0e75eefb47dd6fde2eae
nmajuterence <nmajuterence@gmail.com>
2025-01-15 16:10:33 +0100

    1. Shopping List Manager

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -13,13 +13,13 @@ def main():
 
         if choice == '1':
             # Prompt for and add an item
-            items_to_add = (input('enter the item(s) name (seperated by spaces): ').lower()).split()
+            items_to_add = (input('Enter the item to add: (seperated by spaces): ').lower()).split()
             shopping_list.extend(items_to_add)
             print(f'updated shopping list: {shopping_list}')
             pass
         elif choice == '2':
             # Prompt for and remove an item
-            items_to_remove = (input('enter name(s) to remove item(s) (seperated by spaces): ')).lower().split()
+            items_to_remove = (input('Enter the item to remove: (seperated by spaces): ')).lower().split()
             for item in items_to_remove:
                 if item in shopping_list:
                     shopping_list.remove(item)
