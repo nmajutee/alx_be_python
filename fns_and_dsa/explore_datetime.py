e9d82865ef1c16cfaf8d8ff4fd03f064cb8ec418
nmajuterence <nmajuterence@gmail.com>
2025-01-17 01:53:05 +0100

    2. Explore `datetime` Module

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -12,7 +12,7 @@ def calculate_future_date():
     current_date = datetime.now()
     number_of_days = int(input('Enter the number of days to add to the current date: '))
     future_date = current_date + timedelta(number_of_days)
-    formatted_future_date = current_date.strftime('%Y-%m-%d')
+    formatted_future_date = future_date.strftime('%Y-%m-%d')
     return f'Future date: {formatted_future_date}'
 
 if __name__ == "__main__":
