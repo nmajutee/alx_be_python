5a49d9ad412545b8457de87feedcd4a84b361a00
nmajuterence <nmajutee@gmail.com>
2025-02-09 00:30:58 +0100

    0. Create a Simple Bank Account Class

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,20 @@
+# 
+class BankAccount:
+    def __init__(self, account_balance = 0):
+        self.account_balance = account_balance
+    
+    def deposit(self, amount):
+       if amount > 0:
+           self.account_balance += amount
+           return f"Deposited {amount}. New balance: {self.account_balance}"
+       else:
+           return f"Invalid deposit amount"
+           
+    def withdraw(self, amount):
+        if amount > 0 and amount <= self.account_balance:
+            self.account_balance -= amount
+            return True
+        else:
+            return False
+    def display_balance(self):
+        return f"Current Balance: {self.account_balance}"
